011bd425d4833bd699d2c1c25aef474c0749a147
simex888888-dotcom <simex888888@gmail.com>
2026-02-25 18:22:53 +0300

    Update env.py

--- prop_trading_app/backend/alembic/env.py
+++ prop_trading_app/backend/alembic/env.py
@@ -3,16 +3,12 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import create_engine, pool
 
-from backend.models import Base
-from backend.config import settings
+from models import Base
+from config import settings
 
-# Это конфиг Alembic, берёт значения из alembic.ini
 config = context.config
-
-# ВПРЯМУЮ подставляем sync-URL из настроек приложения
 config.set_main_option("sqlalchemy.url", settings.SYNC_DATABASE_URL)
 
-# Логирование
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
@@ -20,7 +16,6 @@ target_metadata = Base.metadata
 
 
 def run_migrations_offline():
-    """Запуск миграций в offline-режиме (генерация SQL)."""
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -34,7 +29,6 @@ def run_migrations_offline():
 
 
 def run_migrations_online():
-    """Запуск миграций в online-режиме (настоящая БД)."""
     connectable = create_engine(
         config.get_main_option("sqlalchemy.url"),
         poolclass=pool.NullPool,
